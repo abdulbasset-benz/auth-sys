229fd7f9190fe99a20ff5142eb2aa517b1c2e54a
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-11-08 22:36:46 +0100

    Create user registration method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,22 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
 
     public function RegisterForm(): view 
     {
-        return view('register');
+        return view('auth.register');
     }
-    
-    public function registerUser(Request $request){
-        $validatedData = $request->validate([
-           'name' => ['required', 'max:255'],
-           'email' => ['required', ],
-           'password' => ['required' , 'min:4' , 'email' , 'confirmed']
-        ]);
+
+    public function dashboard()
+    {
+        return view('auth.dashboard');
     }
+
+    public function registerUser(Request $request)
+    {
+        // validate
+            $request->validate([
+                'name' => ['required','max:255','string'],
+                'email' => ['required','string', 'email:rfc,dns','max:255','unique:users,email' ],
+                'password' => ['required', 'string', 'min:3', 'confirmed']
+            ]);
+
+            
+        // Register
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+            
+        // Login
+        
+
+        // Redirect
+        return redirect()->route('auth.dashboard');
+}
+
 }
