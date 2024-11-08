b6d9bf25128dfcb2285bc26519311c8ae1164f86
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-11-08 22:45:39 +0100

    Add reusable components login form and navigation and layout page

--- resources/views/auth/welcome.blade.php
+++ resources/views/auth/welcome.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +9,10 @@
     <title>Document</title>
 
 </head>
+
 <body>
-    <h1 class="text-red-500 text-4xl">hello world</h1>
+    <x-forms.register-form />
+
 </body>
+
 </html>
