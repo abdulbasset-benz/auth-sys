8d76b6a97c6d10f97277f5587a8522dd9ae7959c
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-11-08 22:38:58 +0100

    Refactor routes and add missing ones

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.welcome');
 });
 
-Route::get('/register', [UserController::class, 'RegisterForm'])->name('user.register-form');
-Route::post('/signUp', [UserController::class, 'SignUp'])->name('sign-up');
-Route::get('/login', [UserController::class, 'loginForm'])->name('user.login-form');
-Route::post('/login', [UserController::class, 'login'])->name('login');
-Route::get('/dashboard', [UserController::class, 'dashbaord'])->name('user.dashboard');
+Route::get('/register', [UserController::class, 'RegisterForm'])->name('auth.register');
+Route::post('/register', [UserController::class, 'registerUser'])->name('register-user');
+Route::get('/login', [UserController::class, 'loginForm'])->name('login-form');
+Route::post('/login', [UserController::class, 'login'])->name('auth.login');
+Route::get('/dashboard', [UserController::class, 'dashboard'])->name('auth.dashboard');
 Route::post('/logout', [UserController::class, 'logout'])->name('logout');
