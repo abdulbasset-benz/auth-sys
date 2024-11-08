5005860f1690e83b69d50fa86c454383a8a9413b
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-11-08 11:17:29 +0100

    Set up routes for authentication system

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/register', function () {
-    return view('register');
-});
+
+Route::get('/register', [UserController::class, 'RegisterForm'])->name('user.register-form');
+Route::post('/signUp', [UserController::class, 'SignUp'])->name('sign-up');
+Route::get('/login', [UserController::class, 'loginForm'])->name('user.login-form');
+Route::post('/login', [UserController::class, 'login'])->name('login');
+Route::get('/dashboard', [UserController::class, 'dashbaord'])->name('user.dashboard');
+Route::post('/logout', [UserController::class, 'logout'])->name('logout');
