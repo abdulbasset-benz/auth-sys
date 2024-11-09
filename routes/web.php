2b0bd902005d9323c7c066728535740dfa85b55d
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-11-09 22:24:17 +0100

    Generate loginform and user login routes

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 
 Route::get('/register', [UserController::class, 'RegisterForm'])->name('auth.register');
 Route::post('/register', [UserController::class, 'registerUser'])->name('register-user');
-Route::get('/login', [UserController::class, 'loginForm'])->name('login-form');
-Route::post('/login', [UserController::class, 'login'])->name('auth.login');
+Route::get('/login', [UserController::class, 'loginform'])->name('auth.form');
+Route::post('/login', [UserController::class, 'login'])->name('login');
 Route::get('/dashboard', [UserController::class, 'dashboard'])->name('auth.dashboard');
 Route::post('/logout', [UserController::class, 'logout'])->name('logout');
