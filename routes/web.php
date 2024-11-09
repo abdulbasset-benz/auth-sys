0b9b5f726924da775e0f3dcfdb8f400742ce5666
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-11-09 23:55:41 +0100

    Chore: change logout route method from post to get

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,10 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('auth.welcome');
-});
-
+Route::get('/', [UserController::class, 'home'])->name('auth.home');
 Route::get('/register', [UserController::class, 'RegisterForm'])->name('auth.register');
 Route::post('/register', [UserController::class, 'registerUser'])->name('register-user');
 Route::get('/login', [UserController::class, 'loginform'])->name('auth.form');
 Route::post('/login', [UserController::class, 'login'])->name('login');
 Route::get('/dashboard', [UserController::class, 'dashboard'])->name('auth.dashboard');
-Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+Route::get('/logout', [UserController::class, 'logout'])->name('logout');
