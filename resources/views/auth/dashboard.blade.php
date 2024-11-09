f195e759b7ce683012dfab7ed7166dbb96e17543
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-11-09 23:54:50 +0100

    Add navigation bar component

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -1,3 +1,6 @@
+@vite('resources/css/app.css')
+<x-forms.navigation />
 <div>
-    <h1>this is the dashboard page</h1>
+    <h1 class="text-4xl font-bold text-center">this is the dashboard page</h1>
+    <p class="text-4xl font-bold text-center">welcome back, {{ Auth()->user()->name }}</p>
 </div>
