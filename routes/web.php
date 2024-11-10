24bcc80627304357df2a8af3000e9bcd05c998f8
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-11-10 19:53:01 +0100

    Add simple middelware action

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,18 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', [UserController::class, 'home'])->name('auth.home');
-Route::get('/register', [UserController::class, 'RegisterForm'])->name('auth.register');
-Route::post('/register', [UserController::class, 'registerUser'])->name('register-user');
-Route::get('/login', [UserController::class, 'loginform'])->name('auth.form');
-Route::post('/login', [UserController::class, 'login'])->name('login');
-Route::get('/dashboard', [UserController::class, 'dashboard'])->name('auth.dashboard');
-Route::get('/logout', [UserController::class, 'logout'])->name('logout');
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [UserController::class, 'dashboard'])->name('auth.dashboard');
+    Route::get('/logout', [UserController::class, 'logout'])->name('logout');
+});
+
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/register', [UserController::class, 'RegisterForm'])->name('auth.register');
+    Route::post('/register', [UserController::class, 'registerUser'])->name('register-user');
+    Route::get('/login', [UserController::class, 'loginform'])->name('auth.form');
+    Route::post('/login', [UserController::class, 'login'])->name('login');
+});
